18ea1c61661fca050340244e6bfc148e7879c5b2
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-29 15:16:10 -0700

    RealtimeScheduler stubs

--- src/system/RealTimeScheduler.cpp
+++ src/system/RealTimeScheduler.cpp
@@ -1,2 +1,13 @@
 #include "RealTimeScheduler.h"
 
+// currently stubbed
+void RealtimeScheduler::bindThread(std::thread& t, int cpu, int priority)
+{
+	// TODO
+}
+
+// currently stubbed
+void RealtimeScheduler::setWatchdogTimeout(std::chrono::milliseconds timeout)
+{
+	// TODO
+}
