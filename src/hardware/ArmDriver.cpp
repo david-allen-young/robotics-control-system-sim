f515eb8c7e927cd189a1398d5b13fc1f65714304
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-29 15:11:05 -0700

    ArmDriver stubs

--- src/hardware/ArmDriver.cpp
+++ src/hardware/ArmDriver.cpp
@@ -1,1 +1,14 @@
 #include "ArmDriver.h"
+
+// currently stubbed
+void ArmDriver::sendJointTargets(const std::vector<double>& angles)
+{
+	// TODO
+}
+
+// currently stubbed
+std::vector<double> ArmDriver::readJointAngles()
+{
+	// TOOD
+	return {}; // temp
+}
