c8db5fe6f1dcbcf05f81eb6312a0817435ab8fd1
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-05-29 18:16:32 -0700

    stub EncoderReader

---
+++ src/hardware/EncoderReader.cpp
@@ -0,0 +1,9 @@
+#include "EncoderReader.h"
+#include <iostream>
+
+// currently stubbed
+int readEncoder()
+{
+    std::cout << "[EncoderReader] reading encoder value" << std::endl;
+    return 0; // dummy count
+}
